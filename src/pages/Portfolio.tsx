87220e1d7a4a634c11b979d8b85e66216ac2ce5d
fiez alhag <148772477+FiezAlhagwork@users.noreply.github.com>
2025-02-18 12:25:34 +0300

    upload change tsconfig.tsx

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,9 +1,10 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Navigation } from 'swiper/modules';
 import { videoProjects } from '../constant';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+// import 'swiper/css';
+// import 'swiper/css/pagination';
+// import 'swiper/css/navigation'
+import 'swiper/swiper-bundle.css';
 import { useState } from 'react';
 
 
