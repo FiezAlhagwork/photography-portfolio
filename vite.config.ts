135858d218162c361f233e4dbe012612c9fc6432
fiez alhag <148772477+FiezAlhagwork@users.noreply.github.com>
2025-02-22 14:49:54 +0300

    change file

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import compression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),compression({ algorithm: 'brotliCompress' })],
-  build: {
-    minify: "terser",
-  },
+  plugins: [react()],
 })
-
-
